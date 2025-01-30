046f6b741a8e62d96960f70d93e9f1afbf182112
Gustavo Finch <gustavo.finch@gmail.com>
2025-01-30 19:24:00 -0300

    Solucion

--- src/createServer.js
+++ src/createServer.js
@@ -1,37 +1,77 @@
 'use strict';
 
-// const express = require('express');
 const express = require('express');
+const cors = require('cors');
+
+// let expenses = [
+//   {
+//     id: 1,
+//     userId: 1,
+//     spentAt: "2024-01-29T12:00:00.000Z",
+//     title: "Lunch",
+//     amount: 15,
+//     category: "Food2",
+//     note: "Business lunch"
+//   },
+//   {
+//     id: 2,
+//     userId: 1,
+//     spentAt: "2024-01-29T12:00:00.000Z",
+//     title: "Lunch2",
+//     amount: 15,
+//     category: "Food2",
+//     note: "Business lunch2"
+//   },
+//   {
+//     id: 3,
+//     userId: 2,
+//     spentAt: "2024-01-29T12:00:00.000Z",
+//     title: "Lunch3",
+//     amount: 15,
+//     category: "Electronics",
+//     note: "Business lunch3"
+//   }
+// ];
+// let users = [
+//   { name: "John Doe" }
+// ];
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
-  const app = express();
-
+  let expenses = [];
   let users = [];
 
-  app.get('/users', express.json(), async (req, res) => {
+  const app = express();
+
+  app.use(cors());
+
+  app.get('/users', (req, res) => {
+    res.statusCode = 200;
     res.send(users);
   });
 
-  app.get('/users/:id', async (req, res) => {
+  app.get('/users/:id', (req, res) => {
     const { id } = req.params;
 
-    const user = users.find((targetUser) => targetUser.id === +id);
+    if (!id) {
+      res.sendStatus(400);
 
-    if (!user) {
+      return;
+    }
+
+    const newUser = users.find((user) => user.id.toString() === id.toString());
+
+    if (!newUser) {
       res.sendStatus(404);
 
       return;
     }
 
-    res.send(user);
+    res.statusCode = 200;
+    res.send(newUser);
   });
 
-  app.post('/users', express.json(), async (req, res) => {
+  app.post('/users', express.json(), (req, res) => {
     const { name } = req.body;
-    const newId = Math.max(...users.map((item) => item.id), 0) + 1;
 
     if (!name) {
       res.sendStatus(400);
@@ -41,7 +81,7 @@ function createServer() {
 
     const user = {
       name,
-      id: newId,
+      id: users.length + 1,
     };
 
     users.push(user);
@@ -50,167 +90,200 @@ function createServer() {
     res.send(user);
   });
 
-  app.delete('/users/:id', async (req, res) => {
+  app.delete('/users/:id', (req, res) => {
     const { id } = req.params;
 
-    const targetUser = users.find((user) => user.id === +id);
+    if (!id) {
+      res.sendStatus(400);
 
-    if (!targetUser) {
+      return;
+    }
+
+    const newUsers = users.filter(
+      (user) => user.id.toString() !== id.toString(),
+    );
+
+    if (newUsers.length === users.length) {
       res.sendStatus(404);
 
       return;
     }
 
-    const newUsers = users.filter((user) => user.id !== +id);
-
     users = newUsers;
+
     res.sendStatus(204);
   });
 
-  app.patch('/users/:id', express.json(), async (req, res) => {
+  app.patch('/users/:id', express.json(), (req, res) => {
     const { id } = req.params;
     const { name } = req.body;
 
-    const targetUser = users.find((user) => user.id === +id);
+    if (!id) {
+      res.sendStatus(400);
 
-    if (!targetUser) {
+      return;
+    }
+
+    const newUser = users.find((user) => user.id.toString() === id.toString());
+
+    if (!newUser) {
       res.sendStatus(404);
 
       return;
     }
 
-    if (typeof name !== 'string') {
-      res.sendStatus(422);
+    Object.assign(newUser, { name });
 
-      return;
-    }
-
-    Object.assign(targetUser, { name });
-
-    res.send(targetUser);
+    res.statusCode = 200;
+    res.send(newUser);
   });
 
-  let expenses = [];
+  // expenses
 
-  app.get('/expenses', express.json(), async (req, res) => {
-    const { userId, from, to, categories: category } = req.query;
-    let filteredExpenses = [...expenses];
+  app.get('/expenses', (req, res) => {
+    if (Object.keys(req.query).length > 0) {
+      const query = req.query;
 
-    if (userId) {
-      filteredExpenses = filteredExpenses.filter(
-        (expense) => expense.userId === +userId,
-      );
-    }
+      const filteredExpenses = expenses.filter((expense) => {
+        return Object.entries(query).every(([key, value]) => {
+          if (key === 'from') {
+            return new Date(expense.spentAt) >= new Date(value);
+          }
 
-    if (from && to) {
-      filteredExpenses = filteredExpenses.filter((expense) => {
-        const expenseDate = Date.parse(expense.spentAt);
-        const fromDate = Date.parse(from);
-        const toDate = Date.parse(to);
+          if (key === 'to') {
+            return new Date(expense.spentAt) <= new Date(value);
+          }
 
-        return expenseDate >= fromDate && expenseDate <= toDate;
+          if (key === 'categories') {
+            const categoriesArray = value.split(',');
+
+            return categoriesArray.includes(expense.category);
+          }
+
+          return expense[key]?.toString() === value.toString();
+        });
       });
+
+      if (filteredExpenses.length === 0) {
+        res.sendStatus(404);
+
+        return;
+      }
+
+      res.statusCode = 200;
+      res.send(filteredExpenses);
+
+      return;
     }
 
-    if (category) {
-      filteredExpenses = filteredExpenses.filter(
-        (expense) => expense.category === category,
-      );
-    }
-
-    res.send(filteredExpenses);
+    res.statusCode = 200;
+    res.send(expenses);
   });
 
-  app.post('/expenses', express.json(), async (req, res) => {
-    const { userId, spentAt, title, amount, category, note } = req.body;
+  app.get('/expenses/:id', (req, res) => {
+    const { id } = req.params;
 
-    if (
-      typeof userId !== 'number' ||
-      typeof spentAt !== 'string' ||
-      typeof title !== 'string' ||
-      typeof amount !== 'number' ||
-      typeof category !== 'string' ||
-      typeof note !== 'string'
-    ) {
+    if (!id) {
       res.sendStatus(400);
 
       return;
     }
 
-    const assignedUser = users.find((user) => user.id === +userId);
+    const newExpense = expenses.find(
+      (expense) => expense.id.toString() === id.toString(),
+    );
 
-    if (!assignedUser) {
+    if (!newExpense) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    res.statusCode = 200;
+    res.send(newExpense);
+  });
+
+  app.post('/expenses', express.json(), (req, res) => {
+    const { userId, title, amount, category, note, spentAt } = req.body;
+
+    if (!userId || !title || !amount || !category || !note) {
       res.sendStatus(400);
 
       return;
     }
 
-    const newId = Math.max(...expenses.map((item) => item.id), 0) + 1;
-    const newExpense = {
-      id: newId,
+    const userExists = users.some((user) => user.id === userId);
+
+    if (!userExists) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const expense = {
+      id: expenses.length + 1,
       userId,
-      spentAt,
+      spentAt: spentAt ? new Date(spentAt) : new Date(),
       title,
       amount,
       category,
       note,
     };
 
-    expenses.push(newExpense);
+    expenses.push(expense);
 
     res.statusCode = 201;
-    res.send(newExpense);
+    res.send(expense);
   });
 
-  app.get('/expenses/:id', async (req, res) => {
+  app.delete('/expenses/:id', (req, res) => {
     const { id } = req.params;
 
-    const targetExpense = expenses.find((expense) => expense.id === +id);
+    if (!id) {
+      res.sendStatus(400);
 
-    if (!targetExpense) {
+      return;
+    }
+
+    const newExpenses = expenses.filter(
+      (expense) => expense.id.toString() !== id.toString(),
+    );
+
+    if (newExpenses.length === expenses.length) {
       res.sendStatus(404);
 
       return;
     }
 
-    res.send(targetExpense);
-  });
+    expenses = newExpenses;
 
-  app.delete('/expenses/:id', async (req, res) => {
-    const { id } = req.params;
-
-    const targetExpense = expenses.find((expense) => expense.id === +id);
-
-    if (!targetExpense) {
-      res.sendStatus(404);
-
-      return;
-    }
-
-    const updatedExpenses = expenses.filter((expense) => expense.id !== +id);
-
-    expenses = updatedExpenses;
     res.sendStatus(204);
   });
 
-  app.patch('/expenses/:id', express.json(), async (req, res) => {
+  app.patch('/expenses/:id', express.json(), (req, res) => {
     const { id } = req.params;
-    const data = req.body;
+    const updateFields = req.body;
 
-    const targetExpense = expenses.find((expense) => expense.id === +id);
+    if (!id) {
+      res.sendStatus(400);
 
-    if (!targetExpense) {
+      return;
+    }
+
+    const newExpense = expenses.find(
+      (expense) => expense.id.toString() === id.toString(),
+    );
+
+    if (!newExpense) {
       res.sendStatus(404);
 
       return;
     }
 
-    Object.assign(targetExpense, {
-      ...data,
-    });
+    Object.assign(newExpense, updateFields);
 
     res.statusCode = 200;
-    res.send(targetExpense);
+    res.send(newExpense);
   });
 
   return app;
@@ -219,4 +292,3 @@ function createServer() {
 module.exports = {
   createServer,
 };
-Footer;
